f588fb5dd30b33899546ad6221def0f527fe9295
Vijay <Vijay@LAPTOP-7M2IR9K6>
2019-02-12 18:57:31 +0530

    added spring demo project to git

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	 @RequestMapping(value = "/sagar", method = RequestMethod.GET, produces = "application/json")
+	
+	public String home() {
+		return "Spring boot is working wow";
+	}
+}
